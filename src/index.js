db416d7267491b68669caac7ed68ad4bd9259550
GuoFei <guofei0723@gmail.com>
2021-02-03 22:47:28 +0800

    添加地面与柱子

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import bird1 from './assets/bird-frame-1.png'
 import bird2 from './assets/bird-frame-2.png'
 import bird3 from './assets/bird-frame-3.png'
 import bird4 from './assets/bird-frame-4.png'
+import groundImg from './assets/ground.png'
+import pillarImg from './assets/pillar.png'
+
+// 游戏状态
+const GAMESTATE = {
+  // 等待开始
+  waiting: 1,
+  // 游戏中
+  playing: 2,
+  // 游戏结束
+  gameover: 0,
+}
 
 class MyGame extends Phaser.Scene
 {
@@ -16,16 +28,48 @@ class MyGame extends Phaser.Scene
         arcade: {
           // 重力
           gravity: { y: 2000 },
+          // 开启debug
+          debug: true,
         }
       }
     });
+
+    // 游戏状态 默认为等待开始
+    this.gameState = GAMESTATE.waiting
+    // 柱子上下空间的一半
+    this.halfSpace = 100
+  }
+
+  /**
+   * 准备开始
+   */
+  ready ()
+  {
+    this.gameState = GAMESTATE.waiting
+  }
+  /**
+   * 开始游戏
+   */
+  play ()
+  {
+    this.gameState = GAMESTATE.playing
+  }
+  /**
+   * 游戏结束
+   */
+  gameover ()
+  {
+    this.gameState = GAMESTATE.gameover
   }
 
   preload ()
   {
-    this.load.image('bg', bgImg);
+    this.load.image('bg', bgImg)
+    this.load.image('ground', groundImg)
+    this.load.image('pillar', pillarImg)
     // 加载小鸟图片
-    [bird1, bird2, bird3, bird4].forEach((bird, i) => this.load.image(`bird${i + 1}`, bird))
+    const birds = [bird1, bird2, bird3, bird4]
+    birds.forEach((bird, i) => this.load.image(`bird${i + 1}`, bird))
   }
     
   create ()
@@ -34,11 +78,73 @@ class MyGame extends Phaser.Scene
     const logo = this.add.image(200, 0, 'bg');
     logo.setOrigin(0.5, 0).setScale(0.85)
 
+    // 创建柱子
+    const pillars = this.physics.add.group()
+    // 上下柱子空间的一半
+    const { halfSpace } = this
+    for (let i=0; i < 5; i++) {
+      const x = 800 + i * 320
+      // 随机垂直位置
+      const randomY = Phaser.Math.Between(halfSpace * 2, 600 - halfSpace * 2)
+      
+      // 上面的柱子
+      const topPillar = pillars.create(x, randomY - halfSpace, 'pillar')
+      topPillar.setOrigin(0, 1).setGravityY(-2000)
+
+      // 下面的柱子
+      const bottomPillar = pillars.create(x, randomY + halfSpace, 'pillar')
+      bottomPillar.setOrigin(0, 0).setGravityY(-2000)
+    }
+
+    this.pillars = pillars
+
+    // 添加地面
+    const ground = this.physics.add.group({
+      key: 'ground',
+      repeat: 4,
+      setOrigin: {
+        x: 0,
+        y: 1,
+      },
+      setXY: {
+        x: 0,
+        y: 600,
+        stepX: 128,
+      },
+      // 向左移动
+      velocityX: -200,
+      // 抵消世界的重力
+      gravityY: -2000,
+    })
+
+    this.ground = ground
+
+    
+    // 创建一个形状作为静态地面
+    const staticGround = this.add.rectangle(0, 600, 400, 32)
+    staticGround.setOrigin(0, 1)
+    // 让几何形象成为物理对象
+    this.physics.world.enable(staticGround, Phaser.Physics.Arcade.STATIC_BODY)
+
     // 小鸟
     const bird = this.physics.add.sprite(200, 300, 'bird1')
     bird.setScale(0.1).refreshBody()
     // 场景保存bird实例
     this.bird = bird
+
+    // 小鸟与地面碰撞
+    this.physics.add.collider(bird, staticGround, () => {
+      if (this.gameState === GAMESTATE.playing) {
+        this.gameover()
+      }
+    })
+
+    // 小鸟与柱子碰撞
+    this.physics.add.overlap(bird, pillars, () => {
+      if (this.gameState === GAMESTATE.playing) {
+        this.gameover()
+      }
+    })
 
     this.anims.create({
       key: 'fly',
@@ -65,8 +171,12 @@ class MyGame extends Phaser.Scene
     this.physics.pause()
 
     this.input.keyboard.on('keydown', () => {
-      // 开启物理引擎
-      this.physics.resume()
+      if (this.gameState === GAMESTATE.waiting) {
+        // 开启物理引擎
+        this.physics.resume()
+        // 开始游戏
+        this.play()
+      }
       // 设置向上的速度
       bird.setVelocityY(-600)
     })
@@ -78,10 +188,62 @@ class MyGame extends Phaser.Scene
   update()
   {
     // 根据垂直速度播放相应动画
-    if (this.bird.body.velocity.y <= 0) {
+    if (this.bird.body.velocity.y <= 0 && this.gameState !== GAMESTATE.gameover) {
       this.bird.anims.play('fly', true)
     } else {
       this.bird.play('fall', true)
+    }
+
+    // 设置小鸟旋转与角度
+    if (this.bird.body.velocity.y < 0) {
+      // 角速度重置为0，角度重置为0
+      this.bird.setAngularVelocity(0).setAngle(0)
+    } else if (this.bird.body.velocity.y > 0) {
+      // 下落时设置旋转角速度
+      this.bird.setAngularVelocity(180)
+    }
+    // 限定最大旋转度
+    if (this.bird.angle >= 65) {
+      this.bird.setAngularVelocity(0)
+    }
+
+    // 地面循环移动
+    this.ground.children.iterate((block) => {
+      if (block.body.position.x <= -128) {
+        block.body.position.x += this.ground.children.size * 128
+      }
+    })
+
+    // 柱子循环移动
+    for(let i = 0; i < this.pillars.children.size; i += 2) {
+      // 上面的柱子
+      const tp = this.pillars.children.entries[i]
+      // 下面的柱子
+      const bp = this.pillars.children.entries[i + 1]
+      // 超出指定位置后循环到队尾
+      if (tp.body.position.x <= -240) {
+        // 队尾x坐标
+        const x = tp.body.position.x + this.pillars.children.size / 2 * 320
+        // 随机垂直位置
+        const randomY = Phaser.Math.Between(this.halfSpace * 2, 600 - this.halfSpace * 2)
+        // 修改上面的柱子位置
+        tp.setPosition(x, randomY - this.halfSpace)
+        // 修改下面的柱子位置
+        bp.setPosition(x, randomY + this.halfSpace)
+      }
+    }
+
+    if (this.gameState === GAMESTATE.playing) {
+      this.ground.setVelocityX(-200)
+      this.pillars.setVelocityX(-200)
+    }
+    else if (this.gameState === GAMESTATE.gameover) {
+      // 地面停止移动
+      this.ground.setVelocityX(0)
+      this.pillars.setVelocityX(0)
+    }
+    else if (this.gameState === GAMESTATE.waiting) {
+      //
     }
   }
 }
